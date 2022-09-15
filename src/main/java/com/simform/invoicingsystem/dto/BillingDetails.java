b784afdb581076b1fb89a6e055159c8c824b8d4e
Virti Vora <virti.v@simformsolutions.com>
2022-09-15 18:36:51 +0530

    add: add Billing Deatils DTO

---
+++ src/main/java/com/simform/invoicingsystem/dto/BillingDetails.java
@@ -0,0 +1,18 @@
+package com.simform.invoicingsystem.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BillingDetails {
+    private String cycle;
+    private int invoiceTerm;
+    private String payModel;
+    private String type;
+    private LocalDate accStartDate;
+    private String startDate;
+}
