511640b76168214dd18982c4084b4b0213f688e0
Virti Vora <virti.v@simformsolutions.com>
2022-09-15 16:16:17 +0530

    feat: add csm and user entity

--- src/main/java/com/simform/invoicingsystem/entity/Csm.java
+++ src/main/java/com/simform/invoicingsystem/entity/Csm.java
@@ -3,25 +3,23 @@ package com.simform.invoicingsystem.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
-@Table(name = "users")
-public class User {
+public class Csm {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
-    private String email;
-    private String password;
-    private String reset_password_token;
-    private boolean status;
-    private long user_role_id;
+    private long project_id;
     private LocalDateTime created_at;
     private String created_by;
     private LocalDateTime updated_at;
