39b0347d81c418de6c429c83dd9d0d668c415d64
Amit Chavda <amitchavda9879@gmail.com>
2022-09-16 20:15:29 +0530

    Fix: ProjectController response

--- src/main/java/com/simform/invoicingsystem/controller/ProjectController.java
+++ src/main/java/com/simform/invoicingsystem/controller/ProjectController.java
@@ -1,6 +1,6 @@
 package com.simform.invoicingsystem.controller;
 
-import com.simform.invoicingsystem.dto.ClientDetails;
+import com.simform.invoicingsystem.dto.GenericResponse;
 import com.simform.invoicingsystem.dto.ProjectDetails;
 import com.simform.invoicingsystem.service.ProjectService;
 import org.springframework.http.HttpStatus;
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
 
 
 @Controller
@@ -23,8 +25,14 @@ public class ProjectController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<ProjectDetails> addProject(@RequestBody ProjectDetails projectDetails) {
-        System.out.println(projectDetails);
-        return new ResponseEntity<ProjectDetails>(projectDetails, HttpStatus.CREATED);
+    public ResponseEntity<GenericResponse> addProject(@RequestBody ProjectDetails projectDetails) {
+        projectService.addProject(projectDetails);
+
+        GenericResponse genericResponse = new GenericResponse();
+        genericResponse.setMessage("Created");
+        genericResponse.setCode(201);
+        genericResponse.setTimestamp(LocalDateTime.now());
+        genericResponse.setSuccess(true);
+        return new ResponseEntity<>(genericResponse, HttpStatus.CREATED);
     }
 }
