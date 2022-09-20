c95940152d7e698bb72e70218821374ef64d443e
Dhruv <dhruvpatel19201@gmail.com>
2022-09-20 12:42:29 +0530

    Add dropdown apis continue

--- src/main/java/com/simform/invoicingsystem/controller/DropdownController.java
+++ src/main/java/com/simform/invoicingsystem/controller/DropdownController.java
@@ -1,7 +1,43 @@
 package com.simform.invoicingsystem.controller;
 
+import com.simform.invoicingsystem.service.DropdownService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DropdownController {
+
+    private DropdownService dropdownService1;
+
+    public DropdownController(DropdownService dropdownService1) {
+        this.dropdownService1 = dropdownService1;
+    }
+
+    @GetMapping(value = "/getProjectModel")
+    public List<String> getProjectModel(){
+        return dropdownService1.getProjectModel();
+    }
+
+    @GetMapping(value = "/getInvoiceCycle")
+    public List<String> getInvoiceCycle(){
+        return dropdownService1.getInvoiceCycle();
+    }
+
+    @GetMapping(value = "/getAccType")
+    public List<String> getAccType(){
+        return dropdownService1.getAccType();
+    }
+
+    @GetMapping(value = "/getPayModel")
+    public List<String> getPayModel(){
+        return dropdownService1.getPayModel();
+    }
+
+    @GetMapping(value = "/getActiveBilling")
+    public List<String> getActiveBilling(){
+        return dropdownService1.getActiveBilling();
+    }
+
 }
--- src/main/java/com/simform/invoicingsystem/service/DropdownService.java
+++ src/main/java/com/simform/invoicingsystem/service/DropdownService.java
@@ -1,9 +1,53 @@
 package com.simform.invoicingsystem.service;
 
+import com.simform.invoicingsystem.entity.AccType;
+import com.simform.invoicingsystem.entity.InvoiceCycle;
+import com.simform.invoicingsystem.entity.ProjectModel;
+import com.simform.invoicingsystem.repository.AccTypeRepository;
+import com.simform.invoicingsystem.repository.InvoiceCycleRepository;
+import com.simform.invoicingsystem.repository.ProjectModelRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class DropdownService {
+
+    private ProjectModelRepository projectModelRepository;
+    private InvoiceCycleRepository invoiceCycleRepository;
+    private AccTypeRepository accTypeRepository;
+
+    public DropdownService(ProjectModelRepository projectModelRepository, InvoiceCycleRepository invoiceCycleRepository, AccTypeRepository accTypeRepository) {
+        this.projectModelRepository = projectModelRepository;
+        this.invoiceCycleRepository = invoiceCycleRepository;
+        this.accTypeRepository = accTypeRepository;
+    }
+
+    public List<String>  getProjectModel(){
+        List<ProjectModel> projectModelList = projectModelRepository.findAll();
+        return projectModelList.stream().map(ProjectModel::getModel).collect(Collectors.toList());
+    }
+
+    public List<String> getInvoiceCycle(){
+        List<InvoiceCycle> invoiceCycleList = invoiceCycleRepository.findAll();
+        return invoiceCycleList.stream().map(InvoiceCycle::getCycle).collect(Collectors.toList());
+    }
+
+    public List<String> getAccType(){
+        List<AccType> accTypeList = accTypeRepository.findAll();
+        return accTypeList.stream().map(AccType::getAccType).collect(Collectors.toList());
+    }
+
+    public List<String> getPayModel(){
+        return Arrays.asList("Prepaid" , "Postpaid");
+    }
+
+    public List<String> getActiveBilling(){
+        return Arrays.asList("Yes" , "No");
+    }
+
 }
