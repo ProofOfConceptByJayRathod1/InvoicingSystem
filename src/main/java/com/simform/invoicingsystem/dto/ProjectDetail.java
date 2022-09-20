cd55784814b5acccb006d4a1019d448690c28f6f
aesha <aesha.shingala@gmail.com>
2022-09-20 14:14:54 +0530

    Fix: ProjrctDetail Remove: ProjectDetailTest

--- src/main/java/com/simform/invoicingsystem/dto/ProjectDetail.java
+++ src/main/java/com/simform/invoicingsystem/dto/ProjectDetail.java
@@ -1,6 +1,6 @@
 package com.simform.invoicingsystem.dto;
 
-import com.simform.invoicingsystem.validation.EmailValidation;
+import com.simform.invoicingsystem.util.validation.EmailValidation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +8,6 @@ import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDate;
 import java.util.Collection;
-
 
 @Data
 @NoArgsConstructor
