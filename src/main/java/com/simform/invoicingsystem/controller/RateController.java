dea2c9b3ef08cd2f2d6ea8f80586abe0c9958242
Dhruv <dhruvpatel19201@gmail.com>
2022-09-21 17:50:58 +0530

    Add updateRate service and controller

--- src/main/java/com/simform/invoicingsystem/controller/RateController.java
+++ src/main/java/com/simform/invoicingsystem/controller/RateController.java
@@ -1,15 +1,38 @@
 package com.simform.invoicingsystem.controller;
 
+import com.simform.invoicingsystem.dto.GenericResponse;
+import com.simform.invoicingsystem.dto.RateDetails;
 import com.simform.invoicingsystem.service.RateService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
-@RequestMapping("rates")
+@RequestMapping("/rates")
 public class RateController {
     private RateService rateService;
 
     public RateController(RateService rateService) {
         this.rateService = rateService;
+    }
+
+    @Operation(summary = "Update Rate API", description = "Here, some of the rates of project will be update", tags = {"Rate Controller"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rate updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Resource not found"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
+    @PutMapping(value = "/update/{projectName}")
+    public ResponseEntity<GenericResponse> updateRateDetails(@PathVariable("projectName") String projectName , @RequestBody List<RateDetails> rateDetailsList){
+        Collection<RateDetails> rateDetails = rateService.updateRate(projectName , rateDetailsList);
+        return new ResponseEntity<>(new GenericResponse(true, "Updated", rateDetails, 200, LocalDateTime.now()), HttpStatus.OK);
     }
 }
--- src/main/java/com/simform/invoicingsystem/service/RateService.java
+++ src/main/java/com/simform/invoicingsystem/service/RateService.java
@@ -1,14 +1,49 @@
 package com.simform.invoicingsystem.service;
 
+import com.simform.invoicingsystem.dto.RateDetails;
+import com.simform.invoicingsystem.entity.Project;
+import com.simform.invoicingsystem.exception.ResourceNotFoundException;
+import com.simform.invoicingsystem.repository.ProjectRepository;
 import com.simform.invoicingsystem.repository.RateRepository;
+import com.simform.invoicingsystem.repository.TechStackRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class RateService {
 
     private RateRepository rateRepository;
 
-    public RateService(RateRepository rateRepository) {
+    private ProjectRepository projectRepository;
+
+    private TechStackRepository techStackRepository;
+
+
+    public RateService(RateRepository rateRepository, ProjectRepository projectRepository, TechStackRepository techStackRepository) {
         this.rateRepository = rateRepository;
+        this.projectRepository = projectRepository;
+        this.techStackRepository = techStackRepository;
+    }
+
+    public List<RateDetails> updateRate(String projectName, List<RateDetails> rateDetailsList) {
+        Project project = projectRepository.findByName(projectName).orElseThrow(() -> new ResourceNotFoundException("Project with name " + projectName + " not found"));
+
+        project.getRates().forEach(rate -> {
+            rateDetailsList.forEach(rateDetails -> {
+                if (rateDetails.getStack().equalsIgnoreCase(rate.getStack())) {
+                    rate.setRate(rateDetails.getRate());
+                    rate.setUpdatedAt(LocalDateTime.now());
+                    rate.setUpdatedBy(SecurityContextHolder.getContext().getAuthentication().getName());
+                    rateRepository.save(rate);
+                }
+            });
+
+        });
+
+
+        return rateDetailsList;
     }
 }
